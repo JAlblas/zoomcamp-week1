5481e9f7c7ee211ec21108159961d335f5cfe35f
Jasper Alblas <just_jasper@hotmail.com>
2022-04-22 21:16:18 +0200

    made script fetch csv, and be callable from terminal

--- ingest_data.py
+++ ingest_data.py
@@ -19,7 +19,7 @@ def main(params):
     # Download csv
     os.system(f"wget {url} -O {csv_name}")
 
-    engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(csv_name, iterator= True, chunksize = 100000)
 
@@ -49,13 +49,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Ingest csv data to Postgres')
 
-    parser.add_argument('user', help='username for postgres')
-    parser.add_argument('password', help='password for postgres')
-    parser.add_argument('host', help='host for postgres')
-    parser.add_argument('port', help='port for postgres')
-    parser.add_argument('db', help='database name for postgres')
-    parser.add_argument('table_name', help='name of table to write results to')
-    parser.add_argument('url', help='url of the csv file')
+    parser.add_argument('--user', help='username for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of table to write results to')
+    parser.add_argument('--url', help='url of the csv file')
 
     args = parser.parse_args()
     
