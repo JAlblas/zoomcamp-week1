5e0c3ce4ee8062750aad3c458c3fe651545c4d7b
Jasper Alblas <just_jasper@hotmail.com>
2022-05-06 20:45:47 +0200

    Adjusted code for project

--- homework/ingest_data.py
+++ homework/ingest_data.py
@@ -13,28 +13,28 @@ def main(params):
     port = params.port
     db = params.db
     table_name = params.table_name
-    url = params.url
-    csv_name = 'output.csv'
+    #url = params.url
+    trips_csv = 'trips.csv'
+    zones_csv = 'zones.csv'
+
+    trips_url = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+    zone_url = 'https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv'
 
     # Download csv
-    os.system(f"wget {url} -O {csv_name}")
+    os.system(f"wget {trips_url} -O {trips_csv}")
+    os.system(f"wget {zone_url} -O {zones_csv}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name, iterator= True, chunksize = 100000)
+    zone_df = pd.read_csv(zone_url)
+    zone_df.to_sql(name='zones', con=engine, if_exists='replace')
 
-    df = next(df_iter)
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    header_row = pd.read_csv(trips_csv, nrows = 0)
+    header_row.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    for df in pd.read_csv(trips_csv, iterator= True, chunksize = 100000):
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
-
-    while True:
         t_start = time()
-        
-        df = next(df_iter)
         
         df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
         df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', help='port for postgres')
     parser.add_argument('--db', help='database name for postgres')
     parser.add_argument('--table_name', help='name of table to write results to')
-    parser.add_argument('--url', help='url of the csv file')
+    #parser.add_argument('--url', help='url of the csv file')
 
     args = parser.parse_args()
     
